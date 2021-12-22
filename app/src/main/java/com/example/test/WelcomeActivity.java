80dab9b954a87598963bd387347e9298fdafdb54
Monisha Vadivelu <72807111+monisha-pv@users.noreply.github.com>
2021-12-22 13:43:07 +0000

    Initial commit

---
+++ app/src/main/java/com/example/test/WelcomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    public Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        startBtn = (Button) findViewById(R.id.welcome_button_startbtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, CharacterSelectActivity.class);
+                startActivity(intent); //navigate to Character Select Activity page on click start button
+            }
+        });
+    }
+}
+
